ff8c43e28cef57ad9b429b5d653c4797090c61b2
santhosh250101@gmail.com <santhosh250101@gmail.com>
2021-12-23 23:21:42 -0500

    03 GOing All Classic ----------------------- Deleting and construting react structure on own and how to reuse react components

---
+++ one-helloworld/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button.js"
+function App() {
+
+    return (
+        <div>
+            <div style={{ textAlign: "center" }}>Hello World</div>
+            <Button title={"AppStore"} />
+            <Button title={"Google store"} />
+        </div>
+    );
+}
+
+export default App;
---
+++ one-helloworld/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css"
+import Button from "./Button.js";
+import App from "./App.js"
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
